f664112b2115891da61f8fcfac604ae895090c6a
Amruta Kawade <amrutakawade@gmail.com>
2015-06-17 11:38:47 +0530

    Create BubbleSort.py

---
+++ Algorithms/BubbleSort.py
@@ -0,0 +1,19 @@
+"""
+First Pass:
+( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
+( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4
+( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+Time Complexity: O(n*n)
+Auxiliary Space: O(1)
+Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
+"""
+arr = [int(i) for i in input().split()]
+for i in range(0,len(arr)-1):
+    for j in range(0,len(arr)-i-1):
+        if arr[j] > arr[j+1]:
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+    print(arr)
+    
